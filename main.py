16ac705b8cb88fa020f1a50f54216fc9f4555645
v-jomaried <jomaridelafuente@gmail.com>
2021-01-24 16:11:44 +0000

    Imported NEW CSV File

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
-data = pd.read_csv("Environment_Temperature_change_E_All_Data_NOFLAG.csv")
+data = pd.read_csv("netflix_titles.csv")
 
-print(data)
+# Print the head of the netflix_titles data
+print(data.head())
